5333bc22a3828420b90a3e04d63a8ed9d6ce5b29
keserizzet <izzetfurkankeserwtk@gmail.com>
2025-01-12 15:28:09 +0300

    pudate

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-
+import axios from 'axios';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from "simplelightbox";
