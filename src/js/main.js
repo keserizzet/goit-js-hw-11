5d16117cbbc2ca0cb925e9105faaeb6d4262dcaf
keserizzet <izzetfurkankeserwtk@gmail.com>
2025-01-16 13:09:30 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
-// import './css/styles.css';
-// import axios from 'axios';
-// import iziToast from 'izitoast';
-// import 'izitoast/dist/css/iziToast.min.css';
-// import SimpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
+import './css/styles.css';
+import axios from 'axios';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const API_KEY = '48180440-b38c1b4d4768984d9404c0701';
 const BASE_URL = 'https://pixabay.com/api/';
